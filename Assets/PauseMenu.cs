4e08eb93ca34b4b270c195e4198e88185baf32a5
tonyz961 <shengjie.zou@mail.utoronto.ca>
2022-10-08 12:15:38 -0400

    fix main menu

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -25,11 +25,14 @@ public class PauseMenu : MonoBehaviour
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
+        
     }
-    void Pause () {
+    public void Pause () {
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
+        
+        
     }
     public void LoadMenu() {
         Time.timeScale = 1f;
