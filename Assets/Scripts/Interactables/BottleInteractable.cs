65067fcd3dcca88848ac0916bd1b25c975829270
Spencer Murray <spencer@spalmurray.com>
2022-11-16 10:54:13 -0500

    Remove bottle UI

--- Assets/Scripts/Interactables/BottleInteractable.cs
+++ Assets/Scripts/Interactables/BottleInteractable.cs
@@ -7,20 +7,17 @@ public class BottleInteractable : KickableInteractable
 {
     public float maxAmount = 100f;
     public float currentAmount = 0f;
-    public BottleUI uiController;
 
     public void IncreaseAmount(float incrementAmount)
     {
         currentAmount += incrementAmount;
         currentAmount = Math.Clamp(currentAmount, 0f, maxAmount);
-        uiController.UpdateBar(maxAmount, currentAmount);
     }
     
     public void DecreaseAmount(float decrementAmount)
     {
         currentAmount -= decrementAmount;
         currentAmount = Math.Clamp(currentAmount, 0f, maxAmount);
-        uiController.UpdateBar(maxAmount, currentAmount);
 
         if (currentAmount <= 0)
         {
